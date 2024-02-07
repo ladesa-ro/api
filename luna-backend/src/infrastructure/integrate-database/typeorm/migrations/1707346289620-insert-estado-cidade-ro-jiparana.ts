90d908f0bf40b54a5840ac6adbbb7d1dec831bb4
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-02-07 19:36:48 -0400

    feat(backend): typeorm: migrations: insert Ji-Paraná RO

---
+++ luna-backend/src/infrastructure/integrate-database/typeorm/migrations/1707346289620-insert-estado-cidade-ro-jiparana.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InsertEstadoCidadeRoJiparana1707346289620
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      INSERT INTO base_estado ("id", "sigla", "nome") VALUES
+        (11, 'RO', 'Rondônia');
+    `);
+
+    await queryRunner.query(`
+      INSERT INTO base_cidade ("id", "id_estado_fk", "nome") VALUES
+          (1100015, 11, 'Ji-Paraná');
+  `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DELETE FROM base_cidade WHERE 'id' = 1100015;`);
+    await queryRunner.query(`DELETE FROM base_estado WHERE 'id' = 11;`);
+  }
+}
