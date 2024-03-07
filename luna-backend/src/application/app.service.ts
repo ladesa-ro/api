7c82209ef5156d0e7ed10e30a20b4710c0201a36
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-06 23:00:45 -0400

    chore: remove meanless code

--- luna-backend/src/application/app.service.ts
+++ luna-backend/src/application/app.service.ts
@@ -3,18 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    const now = new Date();
-
-    if (now.getUTCHours() >= (21 + 4) % 24 && now.getUTCHours() <= 6 + 4) {
-      return {
-        service: 'sisgea-luna-backend',
-        status: 'up',
-      };
-    } else {
-      return {
-        service: 'sisgea-luna-backend',
-        status: 'up',
-      };
-    }
+    return {
+      service: 'sisgea-luna-backend',
+      status: 'up',
+    };
   }
 }
