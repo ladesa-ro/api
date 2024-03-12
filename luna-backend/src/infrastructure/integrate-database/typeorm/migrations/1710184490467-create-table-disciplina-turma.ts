2d640897f509f200f09d911c6fffb9da9d968a65
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-12 15:43:35 -0400

    feat: migrations: disciplina_turma

--- luna-backend/src/infrastructure/integrate-database/typeorm/migrations/1710184490467-create-table-disciplina-turma.ts
+++ luna-backend/src/infrastructure/integrate-database/typeorm/migrations/1710184490467-create-table-disciplina-turma.ts
@@ -1,8 +1,8 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-const tableName = 'disciplina';
+const tableName = 'disciplina_turma';
 
-export class CreateTableDisciplina1710184490462 implements MigrationInterface {
+export class CreateTableDisciplinaTurma1710184490467 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
@@ -17,16 +17,14 @@ export class CreateTableDisciplina1710184490462 implements MigrationInterface {
           },
 
           //
-
           {
-            name: 'nome',
-            type: 'text',
+            name: 'id_disciplina_fk',
+            type: 'uuid',
             isNullable: false,
           },
-
           {
-            name: 'carga_horaria',
-            type: 'int',
+            name: 'id_turma_fk',
+            type: 'uuid',
             isNullable: false,
           },
           //
@@ -49,16 +47,22 @@ export class CreateTableDisciplina1710184490462 implements MigrationInterface {
             isNullable: true,
           },
         ],
-        foreignKeys: [],
+        foreignKeys: [
+          {
+            name: `fk__${tableName}__depende__disciplina`,
+            columnNames: ['id_disciplina_fk'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'disciplina',
+          },
+          {
+            name: `fk__${tableName}__depende__turma`,
+            columnNames: ['id_turma_fk'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'turma',
+          },
+        ],
       }),
     );
-
-    await queryRunner.query(`
-      CREATE TRIGGER change_date_updated_table_${tableName}
-        BEFORE UPDATE ON ${tableName}
-        FOR EACH ROW
-          EXECUTE FUNCTION change_date_updated();
-    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
