32a15f283370d852bac8840a3fe43ce6cd1613b8
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-13 09:30:23 -0400

    fix: curso.module: import campus/modalidade

--- luna-backend/src/application/business/ensino/curso/curso.module.ts
+++ luna-backend/src/application/business/ensino/curso/curso.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CampusModule } from '../../ambientes/campus/campus.module';
+import { ModalidadeModule } from '../modalidade/modalidade.module';
 import { CursoController } from './curso.controller';
 import { CursoResolver } from './curso.resolver';
 import { CursoService } from './curso.service';
 
 @Module({
-  imports: [],
+  imports: [CampusModule, ModalidadeModule],
   controllers: [CursoController],
   providers: [CursoService, CursoResolver],
   exports: [CursoService],
