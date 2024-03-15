46c0f1b2db3465101fcb60a592bb9071db408de0
Pedro Henrique <pedroauthentico@gmail.com>
2024-03-15 17:06:44 -0400

    feat: calendario-letivo.operations

---
+++ luna-backend/src/application/business/calendario/calendario_letivo/dtos/calendario-letivo.operations.ts
@@ -0,0 +1,6 @@
+import { CALENDARIO_LETIVO_FIND_ONE_BY_ID } from "./calendario-letivo-find-one.operation";
+
+
+export const CalendarioLetivoOperations = {
+    CALENDARIO_LETIVO_FIND_ONE_BY_ID: CALENDARIO_LETIVO_FIND_ONE_BY_ID
+}
