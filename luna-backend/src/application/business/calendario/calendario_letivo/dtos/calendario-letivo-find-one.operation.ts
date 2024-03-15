094c4e23d5773f3f6912492f1481e124acedf074
Pedro Henrique <pedroauthentico@gmail.com>
2024-03-15 17:04:38 -0400

    feat: calendario-letivo-find-one.operation finalizado

--- luna-backend/src/application/business/calendario/calendario_letivo/dtos/calendario-letivo-find-one.operation.ts
+++ luna-backend/src/application/business/calendario/calendario_letivo/dtos/calendario-letivo-find-one.operation.ts
@@ -4,11 +4,69 @@ import { ModalidadeEntity } from 'infrastructure/integrate-database/typeorm/enti
 import * as yup from 'yup';
 import * as Dto from '../../../(spec)';
 import { DtoProperty, ValidationContractUuid, createDtoOperationOptions, createValidationContract, getSchemaField } from '../../../../../infrastructure';
-import { CursoDto, CursoDtoProperties, CursoDtoValidationContract } from './curso.dto';
+import { CalendarioLetivoDto, CalendarioLetivoDtoProperties, CalendarioLetivoDtoValidationContract } from './calendario-letivo.dto';
 
 //====================================================
 
-@ObjectType('CalendarioLetivoFindOneResult')
-export class CalendarioLetivoDto implements Dto.ICalendarioLetivoFindOneResultDto{
-    
+@ObjectType('CalendarioLetivoFindOneResultDto')
+export class CalendarioLetivoFindOneResultDto implements Dto.ICalendarioLetivoFindOneResultDto {
+  @DtoProperty(CalendarioLetivoDtoProperties.CALENDARIO_LETIVO_ID)
+  id!: string;
+
+  //
+
+  @DtoProperty(CalendarioLetivoDtoProperties.CALENDARIO_LETIVO_NOME)
+  nome!: string;
+
+  @DtoProperty(CalendarioLetivoDtoProperties.CALENDARIO_LETIVO_ANO)
+  ano!: number;
+
+  @DtoProperty(CalendarioLetivoDtoProperties.CALENDARIO_LETIVO_CAMPUS_OUTPUT)
+  campus!: CampusEntity;
+
+  @DtoProperty(CalendarioLetivoDtoProperties.CALENDARIO_LETIVO_MODALIDADE_OUTPUT)
+  modalidade!: ModalidadeEntity;
+
+  //
 }
+
+// ======================================================
+
+export const CalendarioLetivoFindOneByIdInputValidationContract = createValidationContract(() =>
+  yup.object().shape({
+    id: getSchemaField(CalendarioLetivoDtoValidationContract(), 'id'),
+  }),
+);
+
+// ======================================================
+
+@InputType('CalendarioLetivoFindOneByIdInputDto')
+export class CalendarioLetivoFindOneByIdInputDto implements Dto.ICalendarioLetivoFindOneByIdInputDto {
+  @DtoProperty(CalendarioLetivoDtoProperties.CALENDARIO_LETIVO_ID)
+  id!: string;
+}
+
+export const CALENDARIO_LETIVO_FIND_ONE_BY_ID = createDtoOperationOptions({
+  description: 'Realiza a consulta a "calendario letivo" por ID.',
+
+  gql: {
+    name: 'calendario-letivoFindOneById',
+
+    inputDtoType: () => CalendarioLetivoFindOneByIdInputDto,
+    inputDtoValidationContract: CalendarioLetivoFindOneByIdInputValidationContract,
+
+    returnType: () => CalendarioLetivoDto,
+  },
+
+  swagger: {
+    returnType: CalendarioLetivoFindOneResultDto,
+
+    params: [
+      {
+        name: 'id',
+        description: 'ID da calendario.',
+        validationContract: ValidationContractUuid,
+      },
+    ],
+  },
+});
