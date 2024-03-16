295a3de443fc7cfef62e03b481ab622e2c028900
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-16 18:51:05 -0400

    feat: migrations: base: arquivo, imagem, imagem_arquivo

---
+++ luna-backend/src/infrastructure/integrate-database/typeorm/migrations/1710029083528-create-imagem_arquivo.ts
@@ -0,0 +1,90 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+const tableName = 'imagem';
+
+export class CreateImagemArquivo1710029021977 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: tableName,
+
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            default: 'gen_random_uuid()',
+          },
+
+          //
+
+          {
+            name: 'largura',
+            type: 'int',
+            isNullable: false,
+          },
+
+          {
+            name: 'altura',
+            type: 'int',
+            isNullable: false,
+          },
+
+          {
+            name: 'formato',
+            type: 'text',
+            isNullable: false,
+          },
+
+          {
+            name: 'mime_type',
+            type: 'text',
+            isNullable: false,
+          },
+
+          //
+
+          {
+            name: 'id_imagem_fk',
+            type: 'uuid',
+            isNullable: false,
+          },
+
+          {
+            name: 'id_arquivo_fk',
+            type: 'uuid',
+            isNullable: false,
+          },
+
+          //
+
+          {
+            name: 'date_created',
+            type: 'timestamptz',
+            isNullable: false,
+            default: 'NOW()',
+          },
+        ],
+
+        foreignKeys: [
+          {
+            name: `fk__${tableName}__depende__imagem`,
+            columnNames: ['id_imagem_fk'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'imagem',
+          },
+          {
+            name: `fk__${tableName}__depende__arquivo`,
+            columnNames: ['id_arquivo_fk'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'arquivo',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(tableName, true, true, true);
+  }
+}
