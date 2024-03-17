20ec8e59a90a773df9b676300f4bd16131ed6854
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-17 07:53:23 -0400

    feat: usuario: find one: vinculosAtivos

--- luna-backend/src/application/business/(spec)/autenticacao/usuario/(dtos)/usuario-find-one/IUsuarioFindOneResultDto.ts
+++ luna-backend/src/application/business/(spec)/autenticacao/usuario/(dtos)/usuario-find-one/IUsuarioFindOneResultDto.ts
@@ -1,3 +1,13 @@
+import { ICampusModel } from '../../../../ambientes';
+import { IUsuarioVinculoCampusModel } from '../../../usuario-vinculo-campus';
 import { IUsuarioModel } from '../../IUsuarioModel';
 
-export interface IUsuarioFindOneResultDto extends Pick<IUsuarioModel, 'id' | 'nome' | 'matriculaSiape' | 'email'> {}
+export interface IUsuarioFindOneResultDtoVinculoAtivoCampus extends Pick<ICampusModel, 'id' | 'razaoSocial' | 'nomeFantasia'> {}
+
+export interface IUsuarioFindOneResultDtoVinculoAtivo extends Pick<IUsuarioVinculoCampusModel, 'id' | 'cargo' | 'ativo'> {
+  campus: IUsuarioFindOneResultDtoVinculoAtivoCampus;
+}
+
+export interface IUsuarioFindOneResultDto extends Pick<IUsuarioModel, 'id' | 'nome' | 'matriculaSiape' | 'email'> {
+  vinculosAtivos: IUsuarioFindOneResultDtoVinculoAtivo[];
+}
