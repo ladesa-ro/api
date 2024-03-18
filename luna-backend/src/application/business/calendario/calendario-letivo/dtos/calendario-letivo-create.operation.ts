7b5ba3645ed8b3d2e0fa0c92582c0094a2ca50af
Pedro Henrique <pedroauthentico@gmail.com>
2024-03-18 14:32:38 -0400

    feat: calendario-letivo-create-operation

--- luna-backend/src/application/business/calendario/calendario-letivo/dtos/calendario-letivo-create.operation.ts
+++ luna-backend/src/application/business/calendario/calendario-letivo/dtos/calendario-letivo-create.operation.ts
@@ -1,11 +1,23 @@
-// ======================================================
 
-import { createDtoOperationOptions } from "infrastructure";
+import { createDtoOperationOptions } from 'infrastructure';
+import { CalendarioLetivoDto, CalendarioLetivoDtoValidationContract } from './calendario-letivo.dto';
+import { CalendarioLetivoFindOneResultDto } from './calendario-letivo-find-one.operation';
 
 export const CALENDARIO_LETIVO_CREATE = createDtoOperationOptions({
     description: 'Realiza o cadastro "calendario-letivo".',
-    gql:{
-        name:'calendario_letivoCreate',
-        
+    gql: {
+        name: 'calendario_letivoCreate',
+        inputDtoType: () => CalendarioLetivoDto,
+
+        inputDtoValidationContract: CalendarioLetivoDtoValidationContract,
+
+        returnType: () => CalendarioLetivoDto,
+    },
+    swagger: {
+        inputBodyType: CalendarioLetivoDto,
+
+        inputBodyValidationContract: CalendarioLetivoDtoValidationContract,
+
+        returnType:CalendarioLetivoFindOneResultDto,
     }
 }) 
