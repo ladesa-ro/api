e332ac5213c8b5c8415dfc0629984cda4450c4cd
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-24 08:15:12 -0400

    fix: calendario-letivo: module: import resolver

--- luna-backend/src/application/business/calendario/calendario-letivo/calendario-letivo.module.ts
+++ luna-backend/src/application/business/calendario/calendario-letivo/calendario-letivo.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CampusModule } from 'application/business/ambientes/campus/campus.module';
 import { ModalidadeModule } from 'application/business/ensino/modalidade/modalidade.module';
 import { CalendarioLetivoController } from './calendario-letivo.controller';
+import { CalendarioLetivoResolver } from './calendario-letivo.resolver';
 import { CalendarioLetivoService } from './calendario-letivo.service';
 
 @Module({
   imports: [CampusModule, ModalidadeModule],
-  providers: [CalendarioLetivoService],
+  providers: [CalendarioLetivoService, CalendarioLetivoResolver],
   controllers: [CalendarioLetivoController],
 })
 export class CalendarioLetivoModule {}
