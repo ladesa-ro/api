ec724bf5b6b63e0713060c693a212815386d3e36
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-13 11:40:49 -0400

    refactor(cidade): use @ladesa-ro/especificacao

--- src/modulos/ambientes/cidade/cidade.resolver.ts
+++ src/modulos/ambientes/cidade/cidade.resolver.ts
@@ -1,28 +1,41 @@
-import { Resolver } from '@nestjs/graphql';
+import LadesaTypings from '@ladesa-ro/especificacao';
+import { Info, Resolver } from '@nestjs/graphql';
+import { GraphQLResolveInfo } from 'graphql';
+import { ContextoDeAcessoGraphQl, IContextoDeAcesso } from '../../../contexto-de-acesso';
+import { CombinedInput, Operation } from '../../../helpers/ladesa';
+import { CidadeService } from './cidade.service';
 
 @Resolver()
 export class CidadeResolver {
-  // constructor(
-  //   //
-  //   private cidadeService: CidadeService,
-  // ) {}
-  // // ========================================================
-  // @Operacao(Spec.CidadeFindAllOperator())
-  // async cidadeFindAll(@Info() info: GraphQLResolveInfo, @ContextoDeAcessoGraphQl() contextoDeAcesso: IContextoDeAcesso, @DadosEntradaGql(Spec.CidadeFindAllOperator()) dto: Spec.IPaginatedInputDto) {
-  //   const selection = getFieldNames(info as any)
-  //     .filter((i) => i.startsWith('data.'))
-  //     .map((i) => i.slice(i.indexOf('.') + 1));
-  //   return this.cidadeService.findAll(contextoDeAcesso, dto, selection);
-  // }
-  // // ========================================================
-  // @Operacao(Spec.CidadeFindOneByIdOperator())
-  // async cidadeFindById(
-  //   @ContextoDeAcessoGraphQl() contextoDeAcesso: IContextoDeAcesso,
-  //   @DadosEntradaGql(Spec.CidadeFindOneByIdOperator())
-  //   dto: LadesaTypings.CidadeFindOneInput,
-  //   @Info() info: GraphQLResolveInfo,
-  // ) {
-  //   const selection = getFieldNames(info as any);
-  //   return this.cidadeService.findByIdStrict(contextoDeAcesso, dto, selection);
-  // }
+  constructor(
+    //
+    private cidadeService: CidadeService,
+  ) {}
+
+  // ========================================================
+
+  @Operation(LadesaTypings.Tokens.Cidade.Operations.List)
+  async cidadeFindAll(
+    //
+    @ContextoDeAcessoGraphQl() contextoDeAcesso: IContextoDeAcesso,
+    @CombinedInput() dto: Spec.IPaginatedInputDto,
+    @Info() info: GraphQLResolveInfo,
+  ) {
+    const selection = getFieldNames(info as any)
+      .filter((i) => i.startsWith('data.'))
+      .map((i) => i.slice(i.indexOf('.') + 1));
+    return this.cidadeService.findAll(contextoDeAcesso, dto, selection);
+  }
+
+  // ========================================================
+  @Operation(LadesaTypings.Tokens.Cidade.Operations.FindById)
+  async cidadeFindById(
+    @ContextoDeAcessoGraphQl() contextoDeAcesso: IContextoDeAcesso,
+    @CombinedInput()
+    dto: LadesaTypings.CidadeFindOneInput,
+    @Info() info: GraphQLResolveInfo,
+  ) {
+    const selection = getFieldNames(info as any);
+    return this.cidadeService.findByIdStrict(contextoDeAcesso, dto, selection);
+  }
 }
