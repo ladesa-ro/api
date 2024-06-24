dda0f4866730e631535cdde322bf67c1f0161cc1
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-24 12:09:01 -0400

    feat(nest/app/http/health): exclude health controller from swagger

--- src/nest-app/adapters/adapter-http/health/health.controller.ts
+++ src/nest-app/adapters/adapter-http/health/health.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 
 @Controller('health')
+@ApiExcludeController()
 export class HealthController {
   @Get('/')
   getHealth() {
