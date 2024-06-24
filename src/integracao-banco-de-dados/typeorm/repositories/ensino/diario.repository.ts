b7192c57351a39e498768758c416d0194d1da95f
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2024-06-24 10:31:35 -0400

    fix: import on diario repository

--- src/integracao-banco-de-dados/typeorm/repositories/ensino/diario.repository.ts
+++ src/integracao-banco-de-dados/typeorm/repositories/ensino/diario.repository.ts
@@ -1,4 +1,4 @@
-import { DiarioEntity } from '../../entities';
+import { DiarioEntity } from '../../entities/ensino/diario.entity';
 import { createRepositoryFactory, IRepositoryFactoryOutput } from '../helpers/create-repository-factory';
 
 export const createDiarioRepository = createRepositoryFactory((ds) => ds.getRepository(DiarioEntity).extend({}));
