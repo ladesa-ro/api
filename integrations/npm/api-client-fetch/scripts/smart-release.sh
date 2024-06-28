536c1a1205d3bd78f15893c8f98af8fc33b8087f
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-28 08:18:40 -0400

    fix(integrations/npm/api-client-fetch/smart-release): handle no changes since latest release

--- integrations/npm/api-client-fetch/scripts/smart-release.sh
+++ integrations/npm/api-client-fetch/scripts/smart-release.sh
@@ -6,17 +6,26 @@ last_tag=$(git describe --tags --abbrev=0)
 last_tag_sha=$(git rev-list -n 1 ${last_tag})
 changes="$(git diff --name-only HEAD ${last_tag_sha} | cat )"
 
-desired=integrations/npm/api-client
+desired=integrations/npm/api-client-fetch
 
 diff_desired=$(echo "${changes}" | grep "${desired}")
 
-diff_desired_count=$( echo "${diff_desired}" | wc -l )
+diff_desired_count=0
+
+if [[ -n "${changes}" ]]; then
+  diff_desired_count=$( echo "${diff_desired}" | wc -l )
+fi
 
 echo "Affected files count: ${diff_desired_count}"
 
 if [[ ${diff_desired_count} -gt 0 ]]; then
   echo "Changed since last release"
-  release-it -c .release-it.json $@
+  
+  if [[ ${SMART_RELEASE_DRY_RUN} == "true" ]]; then
+    echo release-it -c .release-it.json $@
+  else
+    release-it -c .release-it.json $@
+  fi
 else
   echo "No changes since last release"
 fi
