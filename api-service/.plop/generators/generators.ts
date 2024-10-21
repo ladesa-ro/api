85169bc1c2e6ed9e7e3517b503656fb19065390b
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-10-21 22:46:54 +0000

    chore: remove .plop

--- api-service/.plop/generators/generators.ts
+++
@@ -1,8 +0,0 @@
-import type { NodePlopAPI } from "plop";
-import { ModuleCoreGenerator } from "./module-core/module-core.generator";
-
-export const Generators = (plop: NodePlopAPI) => {
-  plop.setGenerator("module-core", ModuleCoreGenerator);
-
-  return plop;
-};
--- api-service/.plop/generators/module-core/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./module-core.generator";
